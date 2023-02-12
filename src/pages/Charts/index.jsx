dc550f9f13b8626b7e035bdc4be72d80a4d9fa97
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-02-12 20:33:21 +0100

    build: replace page placeholders with page links

---
+++ src/pages/Charts/index.jsx
@@ -0,0 +1,8 @@
+export { default as Area } from './Area';
+export { default as Bar } from './Bar';
+export { default as ColorMapping } from './ColorMapping';
+export { default as Financial } from './Financial';
+export { default as Line } from './Line';
+export { default as Pie } from './Pie';
+export { default as Pyramid } from './Pyramid';
+export { default as Stacked } from './Stacked';
