e92198cd67637b272a9a29f52ee715de864c8b00
tmathis <35553152+tsmathis@users.noreply.github.com>
2023-02-13 15:31:01 +0100

    feat: add cards for data on ecommerce page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { links } from "../data/dummy";
+import { links } from "../data/mock_data";
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
