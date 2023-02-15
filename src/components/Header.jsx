dc2f2eda1fadfff934e081ce76fe22d5d6853c77
tmathis <35553152+tsmathis@users.noreply.github.com>
2023-02-15 15:47:35 +0100

    feat: better dark mode support for page bodies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = ({ category, title }) => {
     return (
         <div className="mb-10">
-            <p className="text-gray-400">
+            <p className="text-gray-400 dark:text-white">
                 {category}
             </p>
-            <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+            <p className="text-3xl font-extrabold tracking-tight text-slate-900 dark:text-white">
                 {title}
             </p>
         </div>
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -4,9 +4,9 @@ import { Header } from '../components';
 
 const Customers = () => {
     return (
-        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
             <Header category="Page" title="Customer Base" />
-            <div className="italic">
+            <div className="italic dark:text-white">
                 TO-DO
             </div>
         </div>
